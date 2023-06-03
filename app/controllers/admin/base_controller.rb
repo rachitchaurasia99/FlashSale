0a6f363b8a37d14d79378a2897e2c3514b994955
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-06-04 02:51:07 +0530

    Added a base controller to avoid code repetition

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,7 @@
+class Admin::BaseController < ApplicationController
+  before_action :check_admin
+  
+  def check_admin
+    redirect_to store_homepage_path unless current_user.Admin?
+  end
+end
--- app/controllers/admin/deals_controller.rb
+++ app/controllers/admin/deals_controller.rb
@@ -1,5 +1,4 @@
-class Admin::DealsController < ApplicationController
-  before_action :check_admin
+class Admin::DealsController < Admin::BaseController
   before_action :set_deal, only: %i[show edit update destroy check_publishablity]
   
   def index
@@ -59,10 +58,5 @@ class Admin::DealsController < ApplicationController
 
   def deal_params
     params.require(:deal).permit(:title, :description, :publishable, :quantity, :price_in_cents, :discount_price_in_cents, :publish_date, :published_date, :deals_tax, deal_images_attributes: [:image] )
-  end
-
-  def check_admin
-    redirect_to store_homepage_path unless current_user.Admin?
-  end
-   
+  end 
 end
