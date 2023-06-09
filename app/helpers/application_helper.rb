c8b2316c193b8417c8767c6a3b1c8f754944a8c2
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-06-09 19:28:15 +0530

    updated application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def link_to_add_fields(name, f, association)
-    new_object = f.object.send(association).klass.new
+    new_object = f.object.send(association).class.new
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render(association.to_s.singularize + "_field", f: builder)
