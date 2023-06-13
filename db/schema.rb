53ddf8bf514955615b31158fbf5b97d83759d75a
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-06-13 11:32:52 +0530

    Updated payments and address association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_133458) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_200724) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -109,6 +109,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_133458) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_payments_on_order_id"
+  end
+
+  create_table "refunds", force: :cascade do |t|
+    t.bigint "order_id"
+    t.string "transaction_id"
+    t.string "currency"
+    t.integer "status"
+    t.integer "total_amount_in_cents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_refunds_on_order_id"
   end
 
   create_table "users", force: :cascade do |t|
