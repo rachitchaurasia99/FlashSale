9bcedc50b28a729f4d92b876e3d8e0228cf3e284
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-06-19 12:09:40 +0530

    corrections

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,42 +1,39 @@
 class Deal < ApplicationRecord
-  has_many :deal_images, dependent: :delete_all
+  has_many :deal_images, dependent: :destroy
   accepts_nested_attributes_for :deal_images, allow_destroy: true, reject_if: proc { |attributes| attributes[:image].blank? }
 
-  validates :title, :description, :price_in_cents, :discount_price_in_cents, :quantity, :deals_tax, presence: true
+  validates :title, :description, :price_in_cents, :discount_price_in_cents, :quantity, :deals_tax, :publish_at, presence: true
 
-  validate :valid_publish_date?
-
-  validates :scheduled_deals_count, numericality: { less_than: 2, message: "No more than 2 deals can be published in one day" }, unless: :published_date
+  validates :scheduled_deals_count, numericality: { less_than: MAXIMUM_DEALS_TO_SCHEDULE , message: "No more than 2 deals can be published in one day" }, on: :create
 
   validates :discount_price_in_cents, numericality: { less_than_equal_to: :price_in_cents }
 
-  with_options if: :published_date? do
-    validates :quantity, numericality: { greater_than: 10, message: ' should be greater than 10' }
-    validates :images_count, numericality: { greater_than: 1, message: 'should be greater than 2' }
-    validates :deals_tax, numericality: { in: 0..28 }, allow_blank: true
+  with_options if: :published_at? do
+    validates :quantity, numericality: { greater_than: MINIMUM_DEAL_QUANTITY, message: ' should be greater than 10' }
+    validates :images_count, numericality: { greater_than_or_equal_to: MINIMUM_DEAL_IMAGE , message: 'should be greater than or equal to 2' }
+    validates :deals_tax, numericality: { in: MINIMUM_TAX_RATE..MAXIMUM_TAX_RATE }, allow_blank: true
   end
 
-  scope :live, ->{ where(publishable: true).where.not(published_date: nil) }
-  scope :expired, ->{ where(publishable: false).where.not(published_date: nil) }
-  scope :publishable_on, ->(date) { where(publish_date: date) }
+  validate :valid_publish_at
 
+  scope :live, ->{ where(publishable: true).where.not(published_at: nil) }
+  scope :expired, ->{ where(publishable: false).where.not(published_at: nil) }
+  scope :publishable_on, ->(date) { where(publish_at: date) }
+
+  private
+  
   def images_count
     deal_images.count
   end
 
   def scheduled_deals_count
-    self.class.publishable_on(self.publish_date).size
+    self.class.publishable_on(self.publish_at).size
   end
 
-  def check_publishablity
-    self.publishable = true
-    valid?
-  end
-
-  def valid_publish_date?
-    if publishable && publish_date_was
-      unless publish_date_was - Time.current > TWENTY_FOUR_HOURS
-        errors.add :base, "Cannot update publish_date 24 hours before deal going live"
+  def valid_publish_at
+    if publishable && publish_at_changed?
+      unless publish_at - Time.current > MINIMUM_PUBLISH_TIME_GAP
+        errors.add :base, "Cannot update publish time 24 hours before deal going live"
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  enum role: { customer: 0, Admin: 1 }
+  enum role: { customer: 0, admin: 1 }
 
   validates :first_name, presence: true
 end
