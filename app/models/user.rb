3264b6e66ddfa121e6fafae056ccb6832fc4f5e5
Rachit Chaurasia <62827625+Ratchet1999@users.noreply.github.com>
2023-06-21 20:26:11 +0530

    Revert "User registration"

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-
-  enum role: { customer: 0, admin: 1 }
-
-  validates :first_name, presence: true
-end
