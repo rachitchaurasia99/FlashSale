9f1e01bf9d41d601955a56dad55403beda7fbc77
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-07-07 17:09:47 +0530

    corrections

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,12 @@ class Address < ApplicationRecord
   
   validates :city, :state, :country, :pincode, presence: true
   validates :city, uniqueness: { scope: [:state, :country, :pincode] }
+
+  # before_validation :check_duplicate_address, on: :create
+
+  # scope :find_address,->(city,state,country,pincode,user){ where(city: city, state: state, country: country, pincode: pincode, user: user) }
+
+  # def check_duplicate_address
+  #   errors.add(:base,'Address already exists') unless self.class.find_address(city,state,country,pincode,user).empty?
+  # end
 end
