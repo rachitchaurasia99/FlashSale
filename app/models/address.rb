ce1f4a39c03af2fe845dceb5c15a738c0f18f766
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-07-11 13:51:20 +0530

    validations for address

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,5 +3,16 @@ class Address < ApplicationRecord
   has_many :orders
   
   validates :city, :state, :country, :pincode, presence: true
-  validates :city, uniqueness: { scope: [:state, :country, :pincode] }
+
+  validates :city, uniqueness: { scope: [:state, :country, :pincode], message: ', State, Country and Pincode combination already taken' }, on: :create
+
+  # validate :address_validator, on: :create
+
+  # before_validation :address_validator, on: :create
+
+  def address_validator
+    if Address.find_by(city: city, state: state, country: country, pincode: pincode)
+      errors.add(:base, 'Address already exists')
+    end
+  end
 end
