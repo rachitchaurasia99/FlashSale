337e1127e829052fea853c75cfaaa0a215f9e1e4
Rachit Chaurasia <rachitchaurasia7@gmail.com>
2023-07-11 16:14:13 +0530

    corrections

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,13 +2,17 @@ class Address < ApplicationRecord
   belongs_to :user
   has_many :orders
   
-  validates :city, :state, :country, :pincode, presence: true
+  validates :name, :email, :city, :state, :country, :pincode, presence: true
 
-  validates :city, uniqueness: { scope: [:state, :country, :pincode], message: ', State, Country and Pincode combination already taken' }, on: :create
+  validates :city, uniqueness: { scope: [:state, :country, :pincode], message: ', State, Country and Pincode combination already taken' }, if: :attributes_changed?
 
-  # validate :address_validator, on: :create
+  # validate :address_validator, if: :attributes_changed?
 
-  # before_validation :address_validator, on: :create
+  # before_validation :address_validator, if: :attributes_changed?
+
+  def attributes_changed?
+    city_changed? || state_changed? || country_changed? || pincode_changed?
+  end
 
   def address_validator
     if Address.find_by(city: city, state: state, country: country, pincode: pincode)
